8503356404c9acf9e4a89daa36fad92079e31d9b
Jeroen De Wachter <jeroen.de.wachter@telenet.be>
2013-03-04 13:30:50 +0100

    Added timekeeping & steady state detection to Log

--- Model/Log.cs
+++ Model/Log.cs
@@ -16,23 +16,31 @@ namespace TCPFlow.Model
         public List<DataPacket> delivered = new List<DataPacket>();
 
         private StringBuilder m_history;
+        private Dictionary<int, uint> m_historyTiming; //map index in the m_history string to a time
         private uint m_lastEventTime;
+
+        public uint SteadyStateStart { get; private set; }
+        public uint SteadyStateStop { get; private set; }
 
         public Log()
         {
             m_history = new StringBuilder();
+            m_historyTiming = new Dictionary<int, uint>();
 
             m_lastEventTime = uint.MaxValue;
         }
 
         public void ClearHistory()
         {
+            m_historyTiming.Clear();
             m_history.Clear();
             m_lastEventTime = uint.MaxValue;
         }
 
         private void AddToHistory(string str)
         {
+            m_historyTiming.Add(m_history.Length, Timing.Time);
+
             if (m_lastEventTime != uint.MaxValue)
                 m_history.Append(Timing.Time - m_lastEventTime);
 
@@ -40,6 +48,23 @@ namespace TCPFlow.Model
             m_lastEventTime = Timing.Time;
 
             //TODO: add steady state detection
+            string history = m_history.ToString();
+            int l = 1, len = history.Length, middle = len/2;
+            bool found = false;
+            while (!found && l < middle)
+            {
+                string substr = history.Substring(len - l);
+                if (substr.Contains('P') &&
+                    substr.Contains('A') &&
+                    substr.Contains('D') &&
+                    history.Substring(len-2*l, l).Equals(substr))
+                {
+                    SteadyStateStart = m_historyTiming[len - 2 * l];
+                    SteadyStateStop = m_historyTiming[len - l];
+                    //steady state detected!
+                }
+                ++l;
+            }
         }
 
         public void Reset()
